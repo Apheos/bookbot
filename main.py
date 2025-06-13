7300c3db2fb40ce240bb8306eaf3bccde75d3746
Apheos <gabriel.kasiarz@gmail.com>
2025-06-12 19:38:07 -0700

    Function defined for getting word count of book; moved to new file stats.py off of main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-def get_book_text(filepath):
+def get_book_text(filepath): # Reading txt file and passing into string 
 	with open(filepath) as f:
 		file_contents = f.read()
 		return file_contents
 
-def main():
+def main(): # Function for printing text from book
 	text = get_book_text("/home/gabe/workspace/github.com/bookbot/books/frankenstein.txt")
 	print(text)
-main()
 
+# main() # Calling function to print contents of book
+text = get_book_text("/home/gabe/workspace/github.com/bookbot/books/frankenstein.txt")
+
+from stats import word_count
+
+word_count(text)
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_count(book_text): # To find word count of a book
+        num_words = book_text.split() 
+        print(len(num_words), "words found in the document")
