6edc548a68d28c5f6dcccd31c3091b049ccc75de
Apheos <gabriel.kasiarz@gmail.com>
2025-06-15 13:47:40 -0700

    Created some new functions and removed hard-coded paths

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-def get_book_text(filepath): # Reading txt file and passing into string 
+# Importing functions from stats.py
+import sys
+from stats import word_count
+from stats import total_characters
+from stats import sorted_characters
+
+book_path = "/home/gabe/workspace/github.com/bookbot/books/frankenstein.txt"
+
+def get_book_text(filepath): # Reading txt file and passing into string
 	with open(filepath) as f:
 		file_contents = f.read()
 		return file_contents
 
 def main(): # Function for printing text from book
-	text = get_book_text("/home/gabe/workspace/github.com/bookbot/books/frankenstein.txt")
+	text = get_book_text(book_path)
 	print(text)
 
-# main() # Calling function to print contents of book
-text = get_book_text("/home/gabe/workspace/github.com/bookbot/books/frankenstein.txt")
+text = get_book_text(book_path)
+total_chars = total_characters(text)
 
-from stats import word_count
-
-word_count(text)
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_path}...")
+print("----------- Word Count ----------")
+print(f"Found {word_count(text)} total words")
+print("--------- Character Count -------")
+sorted_chars = sorted_characters(total_chars)
+for char_dict in sorted_chars:
+    if char_dict["char"].isalpha():
+        print(f"{char_dict['char']}: {char_dict['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
 def word_count(book_text): # To find word count of a book
-        num_words = book_text.split() 
-        print(len(num_words), "words found in the document")
+	num_words = book_text.split()
+	print(len(num_words),"words found in this document")
+	return len(num_words)
+
+def total_characters(book_text):
+	total_chars_dict = {}
+	for chars in book_text:
+		chars = chars.lower() # Converting characters to lowercase
+		if chars not in total_chars_dict:
+			total_chars_dict[chars] = 1 # Creating dictionary key
+		else: total_chars_dict[chars] += 1 # Incrementing existing keys
+	return total_chars_dict
+
+def sorted_characters(total_chars_dict):
+	sorted_list = [] # Creating an empty list to then sort later
+	for chars in total_chars_dict:
+		count = total_chars_dict[chars]
+		sorted_dict = {
+			"char": chars,
+			"num": count,
+		}
+		sorted_list.append(sorted_dict)
+	sorted_list.sort(reverse=True, key=sort_on) # Sorting the list
+	return sorted_list
+
+def sort_on(item): # Defining function for sorting
+	return item["num"]
+
